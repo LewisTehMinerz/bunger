2e2ae5edaeebc654f17d4f1342e0dd85bbbb9736
Lewis Crichton <lewistehminerz@outlook.com>
2021-06-22 20:45:12 +0100

    bonk commands bunger

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Mutex } from 'async-mutex';
 
 import fs from 'fs';
 
-const config = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
+let config = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
 const bot = new Eris.Client(config.token);
 
 const cooldownMutex = new Mutex();
@@ -18,7 +18,35 @@ bot.on('messageCreate', async msg => {
         channel = bot.getChannel(msg.channel.id) as Eris.TextChannel;
     }
 
-    if (msg.content.toLowerCase().includes('bunger')) {
+    if (msg.content.startsWith('-bonk ') && msg.author.id === config.botOwner) {
+        const bonk: string[] = [];
+        for (const mention of msg.mentions) {
+            bonk.push(mention.id);
+        }
+
+        config.bonked.push(...bonk);
+        fs.writeFileSync('./config.json', JSON.stringify(config, null, 4));
+
+        return await channel.createMessage({
+            content: 'bonked ' + bonk.join(', ')
+        });
+    }
+
+    if (msg.content.startsWith('-unbonk ') && msg.author.id === config.botOwner) {
+        const unbonk: string[] = [];
+        for (const mention of msg.mentions) {
+            unbonk.push(mention.id);
+        }
+
+        config = config.bonked.filter((b: string) => !unbonk.includes(b));
+        fs.writeFileSync('./config.json', JSON.stringify(config, null, 4));
+
+        await channel.createMessage({
+            content: 'unbonked ' + unbonk.join(', ')
+        });
+    }
+
+    if (msg.content.toLowerCase().includes('bunger') && !config.bonked.includes(msg.author.id)) {
         await cooldownMutex.runExclusive(async () => {
             if (cooldowns.has(channel.id)) return;
 
